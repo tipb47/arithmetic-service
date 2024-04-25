938df5489c5c96bbfafa27776a2828c89b8d2f3b
tipb47 <tipb47@gmail.com>
2024-04-25 16:37:07 -0400

    burhrward

--- src/arithmetica.js
+++ src/arithmetica.js
@@ -1,20 +1,6 @@
-function add(n, m){
+
+function add(n, m) {
     return n + m;
 }
 
-function subtract(n, m){
-    return n - m;
-}
-
-function multiply(n, m){
-    return n * m;
-}
-
-function divide(n, m){
-    return n / m;
-}
-
-module.exports = { add };
-module.exports = { subtract };
-module.exports = { multiply };
-module.exports = { divide };
+module.exports = { add }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 require('dotenv').config()
 const express = require('express');
-const cors = require('cors');
-const {add} = require("./arithmetica.js");
-const {subtract} = require("./arithmetica.js");
-const {multiply} = require("./arithmetica.js");
-const {divide} = require("./arithmetica.js");
+const cors = require('cors'); 
+const { add } = require("./arithmetica");
 const app = express();
-app.use(cors());
+app.use(cors()); 
 
-if(!process.env.PORT){
-    throw new Error('Please specify port number.');
+if (!process.env.PORT) {
+  throw new Error(`Please specify the 
+  port number for the HTTP server with the environment variable PORT.`);
 }
 
-const port = process.env.PORT;
-
+const port = process.env.PORT; 
 app.get('/', (req, res) => {
-    res.send('Arithmetic service - Hello World!');
-});
-
+  res.send('Arithmetic service - last updated 3/4/2022');
+}); 
 app.get('/add/:n/:m', (req, res) => {
-    let n = Number(req.params.n);
-    let m = Number(req.params.m);
-    let sum = add(n, m);
+  let n = Number(req.params.n);
+  let m = Number(req.params.m);
+  let sum = add(n, m);
     res.json(sum);
-});
+  }); 
+ 
+app.listen(port);
+
 
 app.get('/sub/:n/:m', (req, res) =>{
     let n = Number(req.params.n);
