2854c3d9e2375c066989c57ef7d6d27e2e89e4e9
tipb47 <tipb47@gmail.com>
2024-04-25 16:32:46 -0400

    kms

--- src/arithmetica.js
+++ src/arithmetica.js
@@ -1,5 +1,20 @@
-function add(n,m) {
-    return n + m
+function add(n, m){
+    return n + m;
 }
 
-module.exports = {add}
+function subtract(n, m){
+    return n - m;
+}
+
+function multiply(n, m){
+    return n * m;
+}
+
+function divide(n, m){
+    return n / m;
+}
+
+module.exports = { add };
+module.exports = { subtract };
+module.exports = { multiply };
+module.exports = { divide };
--- src/index.js
+++ src/index.js
@@ -1,24 +1,54 @@
 require('dotenv').config()
 const express = require('express');
-const cors = require('cors'); 
-const { add } = require("./arithmetica");
+const cors = require('cors');
+const {add} = require("./arithmetica.js");
+const {subtract} = require("./arithmetica.js");
+const {multiply} = require("./arithmetica.js");
+const {divide} = require("./arithmetica.js");
 const app = express();
-app.use(cors()); 
+app.use(cors());
 
-if (!process.env.PORT) {
-  throw new Error(`Please specify the 
-  port number for the HTTP server with the environment variable PORT.`);
+if(!process.env.PORT){
+    throw new Error('Please specify port number.');
 }
 
-const port = process.env.PORT; 
+const port = process.env.PORT;
+
 app.get('/', (req, res) => {
-  res.send('Arithmetic service - last updated 3/4/2022');
-}); 
+    res.send('Arithmetic service - Hello World!');
+});
+
 app.get('/add/:n/:m', (req, res) => {
-  let n = Number(req.params.n);
-  let m = Number(req.params.m);
-  let sum = add(n, m);
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let sum = add(n, m);
     res.json(sum);
-  }); 
- 
+});
+
+app.get('/sub/:n/:m', (req, res) =>{
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let diff = subtract(n, m);
+    res.json(diff);
+})
+
+app.get('/mul/:n/:m', (req, res) =>{
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let prod = multiply(n, m);
+    res.json(prod);
+})
+
+app.get('/div/:n/:m', (req, res) =>{
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let quo = divide(n, m);
+    res.json(quo);
+})
+
+app.get('/prime/:n', (req, res) => {
+    
+    res.json();
+})
+
 app.listen(port);
