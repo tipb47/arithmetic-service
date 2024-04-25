70fd81e09aedc46646fe00677b1ebfb9a3e2f5a7
tipb47 <tipb47@gmail.com>
2024-04-25 16:42:49 -0400

    hahahaa

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-require('dotenv').config()
 const express = require('express');
 const cors = require('cors'); 
 const { add } = require("./arithmetica");
 const app = express();
 app.use(cors()); 
 
-if (!process.env.PORT) {
-  throw new Error(`Please specify the 
-  port number for the HTTP server with the environment variable PORT.`);
-}
-
-const port = process.env.PORT; 
+const port = 5000; 
 app.get('/', (req, res) => {
   res.send('Arithmetic service - last updated 3/4/2022');
 }); 
